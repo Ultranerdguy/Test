66f788c0f321fb6e20b15cfd8c109091b761060a
Sam Clifton <samclifton1729@gmail.com>
2023-03-10 14:49:48 +0000

    Updated main, renamed debug utility file

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <thread>
+#include "util/debug.hpp"
+
+class App
+{
+public:
+  int Run() noexcept
+  {
+    try
+    {
+      MainLoop();
+    }
+    catch(const std::exception& e)
+    {
+      Log(e.what());
+      return 1;
+    }
+    catch (...)
+    {
+      DebugLog("Unknown exception caught");
+      return -1;
+    }
+    return 0;
+  }
+private:
+  void MainLoop()
+  {
+    while (m_running)
+    {
+      
+    }
+  }
+
+  std::atomic_bool m_running = true;
+};
 
 int main()
 {
-  return 0;
+  App mainGame;
+  return mainGame.Run();
 }
