0a04d05fa3727f28b993a55d3504e53fb147d18a
Sam Clifton <samclifton1729@gmail.com>
2023-08-04 15:31:40 +0100

    Moved notify;renamed report;removed VotV reference

--- packages/terminal/src/linux/identifier.c
+++ packages/terminal/src/linux/identifier.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 tlTerminalWindow* tlGetCurrentTerminal()
-  tlTerminalWindow* pRet = (tlTerminalWindow*)malloc(sizeof(tlTerminalWindow));
+  tlTerminalWindow* pRet = malloc(sizeof(*pRet));
   pRet->m_fileOut = STDOUT_FILENO;
   pRet->m_fileIn = STDIN_FILENO;
   return pRet;
